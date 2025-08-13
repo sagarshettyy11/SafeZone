c09c419077e488085ec8f96c629d744138b0c8a8
Sagar Shetty <sagarshettyy11@gmail.com>
2025-08-13 15:40:59 +0530

    Some basic changes

--- safezone/lib/login_page.dart
+++ safezone/lib/login_page.dart
@@ -164,7 +164,7 @@ class _LoginPageState extends State<LoginPage> {
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blue,
                       shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
                     onPressed: _login,
